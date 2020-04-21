d8ff919517447814a7704c6c2d43030aee7fd2d3
Kornienko <kornienkokolya23@gmail.com>
2020-04-21 17:25:59 +0300

    Добавил пару свойств и сделал два обьекта

---
+++ Scripts/Weight.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Builder
+{
+    public static class Weight
+    {
+        public static readonly Dictionary<ObjectType, int> Weights = new Dictionary<ObjectType, int>
+        {
+            {ObjectType.Capsule, 10},
+            {ObjectType.Cube,5 }
+        };
+    }
+}
