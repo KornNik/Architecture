900686861b64956b084afa90429928c528a38448
Kornienko <kornienkokolya23@gmail.com>
2020-04-29 20:07:44 +0300

    Добавил слушателя для аудио и еще один обьект

---
+++ Observer/ListenerHitAudio.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class ListenerHitAudio
+    {
+        public void Add(IAudio value)
+        {
+            value.OnHitAudio += ValueOnHitAudio;
+        }
+
+        public void Remove(IAudio value)
+        {
+            value.OnHitAudio -= ValueOnHitAudio;
+        }
+
+        private void ValueOnHitAudio(AudioSource audioSource)
+        {
+            audioSource.Play();
+        }
+    }
+}
---
+++ Observer/NPC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace Assets.Scripts
+{
+    public sealed class NPC : MonoBehaviour, IHit, IAudio
+    {
+        public event Action<float> OnHitChange = delegate (float f) { };
+        public event Action<AudioSource> OnHitAudio = delegate (AudioSource audioSource) { };
+
+        public void Hit(float damage)
+        {
+            OnHitChange.Invoke(damage);
+        }
+
+        public void HitAudio(AudioSource audioSource)
+        {
+            OnHitAudio.Invoke(audioSource);
+        }
+    }
+}
